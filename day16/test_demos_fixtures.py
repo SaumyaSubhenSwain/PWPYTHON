2298309893b8adaf9d47cce3b4b9c083d0fea4c7
SaumyaSubhenSwain <143710093+SaumyaSubhenSwain@users.noreply.github.com>
2025-12-15 23:24:48 +0530

    Update test_demos_fixtures.py

--- day16/test_demos_fixtures.py
+++ day16/test_demos_fixtures.py
@@ -7,11 +7,11 @@ def setup():
     print("setup browser..")
 
 def test_one(setup):
-    print("this is my test one")
+    print("This is my test one")
 
 def test_two(setup):
-    print("this is my test two")
+    print("This is my test two")
 
 def test_three(setup):
-    print("this is my test three")
+    print("This is my test three")
 
