eddf9f510b2e75dcedda1e4d557467bb49c8c2d8
QA-saumyasubhen <swainsaumyasubhen@gmail.com>
2025-12-17 23:18:20 +0530

    write few test cases...

--- day19/test_pwlocatorsdemo.py
+++ day19/test_pwlocatorsdemo.py
@@ -1,7 +1,40 @@
+import re
+import time
+
 from playwright.sync_api import Page, expect
 
+# 1) page.get_by_alt_text()
+# 2) page.get_by_text()
+# 3) page.get_by_role()
+# 4) page.get_by_label()
+# 5) page.get_by_placeholder()
+# 6) page.get_by_title()
+# 7) page.get_by_test_id()
 
 def test_verifypwlocators(page:Page):
     page.goto("https://demo.nopcommerce.com/")
+    #time.sleep(5)  #seconds
+    page.wait_for_timeout(5000)   #5000ms = 5sec
+
+    # 1) page.get_by_alt_text()
     logo= page.get_by_alt_text("nopCommerce demo store")
     expect(logo).to_be_visible()
+
+    # 2) page.get_by_text()
+    expect(page.get_by_text("Welcome to our store")).to_be_visible()   # full text
+    expect(page.get_by_text("Welcome to")).to_be_visible()   # partial text
+    expect(page.get_by_text(re.compile(".*Welcome.*"))).to_be_visible()   # reg expression
+
+    # 3) page.get_by_role()
+    page.goto("https://demo.nopcommerce.com/register?returnUrl=%2F")
+    page.wait_for_timeout(5000)  # 5000ms = 5sec
+    expect(page.get_by_role("heading", name="Register")).to_be_visible()
+
+    # 4) page.get_by_label()
+    page.get_by_label("First name:").fill("John")
+    page.get_by_label("Last name:").fill("kinedy")
+    page.get_by_label("Email:").fill("abc@gmail.com")
+    page.wait_for_timeout(5000)
+
+
+    page.close()
