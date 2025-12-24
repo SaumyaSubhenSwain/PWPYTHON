1b7c7d97cf025d7013a69f0d08579a7c53d1caa2
SaumyaSubhenSwain <swainsaumya413@gmail.com>
2025-12-24 14:00:24 +0530

    Here I am writing test cases for xpath locators...

--- day21/test_dynamicxpaths.py
+++ day21/test_dynamicxpaths.py
@@ -18,3 +18,22 @@ def test_handle_dynamic_elements(page: Page):
     #         button=page.locator("//button[text()='START' or text()='STOP']")
     #         button.click()
     #         page.wait_for_timeout(2000)
+
+    # using css...............
+    # def test_handle_dynamic_elements_css(page: Page):
+    #     page.goto("https://testautomationpractice.blogspot.com/")
+    #
+    #     for i in range(5):
+    #         button=page.locator("button[name^='st']")
+    #         button.click()
+    #         page.wait_for_timeout(2000)
+
+    # using playwright locator...............
+
+def test_handle_dynamic_elements_css(page: Page):
+    page.goto("https://testautomationpractice.blogspot.com/")
+
+    for i in range(5):
+        button=page.get_by_role("button",name=re.compile(r'ST.*'))
+        button.click()
+        page.wait_for_timeout(2000)
