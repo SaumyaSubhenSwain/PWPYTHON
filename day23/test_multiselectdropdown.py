d5637e9d92df87eaa3d19939d8d35fed5c599395
SaumyaSubhenSwain <swainsaumya413@gmail.com>
2026-01-03 19:25:02 +0530

    Here I am writing test cases for multi select dropdown....

--- day23/test_multiselectdropdown.py
+++ day23/test_multiselectdropdown.py
@@ -9,10 +9,10 @@ def test_single_select_dropdown(page: Page):
     # select multiple options from teh dropdown - 3 ways
 
     # page.locator("#colors").select_option(["Red", "Blue", "Green"])       # By label
-    page.locator("#colors").select_option(label= ["Red", "Blue", "Green"])  # By label
+    # page.locator("#colors").select_option(label= ["Red", "Blue", "Green"])  # By label
 
-    page.locator("#colors").select_option(value=["red","white","green"])    #By value
-    page.locator("#colors").select_option(index=[2,4])      #by index
+    # page.locator("#colors").select_option(value=["red","white","green"])    #By value
+    # page.locator("#colors").select_option(index=[2,4])      #by index
 
     dropdown_options = page.locator("#colors>option")
     expect(dropdown_options).to_have_count(7)
