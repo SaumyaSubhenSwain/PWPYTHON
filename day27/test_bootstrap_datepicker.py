003c7eabb6996a3d8a2c4d797cd3986810098e54
SaumyaSubhenSwain <swainsaumya413@gmail.com>
2026-01-15 09:59:28 +0530

    Here I am writing test cases for bootstrap_datepicker....

--- day27/test_bootstrap_datepicker.py
+++ day27/test_bootstrap_datepicker.py
@@ -1,53 +1,57 @@
-from playwright.sync_api import Page, expect
+from playwright.sync_api import sync_playwright, expect, Page
 
-
-def select_checkin_date(page: Page, year: str, month: str, day: str):
+def select_checkin_date(page, year, month, day):
     while True:
         checkin_month_year = page.locator("h3[aria-live='polite']").nth(0).inner_text()
-        current_month, current_year= checkin_month_year.split()
+        current_month, current_year = checkin_month_year.split(" ")
 
         if current_month == month and current_year == year:
             break
+        else:
+            page.locator('button[aria-label="Next month"]').click()
 
-        page.get_by_label("Next month").click()
-
-    # Select valid dates only
-    dates= page.locator("table.b8fcb0c66a tbody").nth(0).locator("td").all()
-
-    for i in range(dates.count()):
-        if dates.nth(i).text_content() == day:
-            dates.nth(i).click()
+    all_dates = page.locator('table.b8fcb0c66a tbody').nth(0).locator('td').all()
+    for date in all_dates:
+        if date.inner_text() == day:
+            date.click()
             break
 
-def select_checkout_date(page: Page, year: str, month: str, day: str):
+
+
+def select_checkout_date(page, year, month, day):
     while True:
-        checkout_month_year= page.locator("h3[aria-live='polite']").nth(1).inner_text()
-        current_month, current_year= checkout_month_year.split()
+        checkout_month_year = page.locator("h3[aria-live='polite']").nth(1).inner_text()
+        current_month, current_year = checkout_month_year.split(" ")
 
         if current_month == month and current_year == year:
             break
+        else:
+            page.locator('button[aria-label="Next month"]').click()
 
-        page.get_by_label("Next month").click()
-
-    dates= page.locator("table.b8fcb0c66a tbody").nth(1).locator("td").all()
-
-    for i in range(dates.count()):
-        if dates.nth(i).text_content() == day:
-            dates.nth(i).click()
+    all_dates = page.locator('table.b8fcb0c66a tbody').nth(1).locator('td').all()
+    for date in all_dates:
+        if date.inner_text() == day:
+            date.click()
             break
 
-def test_bootstrap_datepicker(page:Page):
+
+
+def test_booking_Date_picker(page: Page):
+
     page.goto("https://www.booking.com/")
-    page.get_by_test_id("searchbox-dates-container").click()    # Clicked on the datepicker
+    page.locator("button[aria-label='Dismiss sign-in info.']").click()
+    page.locator("button[data-testid='searchbox-dates-container']").click()  # clicked on the date picker
 
-    select_checkin_date(page,"2026","January","14")
-    select_checkout_date(page,"2026","February","5")
+    select_checkin_date(page, "2026", "January", "15")
+    select_checkout_date(page, "2026", "February", "5")
 
-    checkin_text= page.locator("span[data-testid ='date-display-field-start']").inner_text()
-    checkout_text= page.locator("span[data-testid ='date-display-field-end']").inner_text()
+    checkin_text=page.locator("span[data-testid='date-display-field-start']").inner_text()
+    checkout_text=page.locator("span[data-testid='date-display-field-end']").inner_text()
 
-    print("Check-in date====>", checkin_text)
-    print("Check-out date====>",checkout_text)
+    print("Check-in date:===>", checkin_text)
+    print("Check-out date: ===>", checkout_text)
 
-    expect(page.locator("span[data-testid ='date-display-field-start']")).to_contain_text(checkin_text)
-    expect(page.locator("span[data-testid ='date-display-field-end']")).to_contain_text(checkout_text)
+    expect(page.locator("span[data-testid='date-display-field-start']")).to_contain_text(checkin_text)
+    expect(page.locator("span[data-testid='date-display-field-end']")).to_contain_text(checkout_text)
+
+    page.wait_for_timeout(5000)
